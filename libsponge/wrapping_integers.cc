20c17a3f1f51d3e4ee6a7cc16274a965ccf8f89e
icettt <329512443@qq.com>
2022-08-19 21:19:35 +0800

    wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -13,11 +13,7 @@ using namespace std;
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
-
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {  return WrappingInt32(     isn.raw_value() + n- ( (n>>32) << 32 )   ) ;}
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
 //! \param isn The initial sequence number
@@ -28,7 +24,31 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! runs from the local TCPSender to the remote TCPReceiver and has one ISN,
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
-uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint)
+{   
+         int32_t dif= n-isn;
+         uint64_t x;
+         if(dif<0)
+           x=0xffffffff+dif+1;
+         else
+           x=dif;
+        uint64_t times=checkpoint>>32;
+        x=(times<<32)+x;
+        if(x<checkpoint)
+        {
+            if(checkpoint -x > x+(1ul<<32) -checkpoint)
+               return x+(1ul<<32);
+            else
+               return x;
+        }
+        else if(x>checkpoint)
+        {
+             if(times==0)
+               return x;
+             if(x- checkpoint > checkpoint - (x -(1ul<<32)))
+               return x-(1ul<<32);
+             else
+               return x;
+        }
+        return x;
 }
